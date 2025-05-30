7532db1a8b3d138a96661685874fe03c570d1263
Shappo1206 <shappocoding@gmail.com>
2025-05-30 23:40:38 +0800

    05302340

---
+++ src/main/java/com/example/demo/model/entity/Hello.java
@@ -0,0 +1,15 @@
+package com.example.demo.model.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+	public class Hello {
+	    @Id @GeneratedValue
+	    private Long id;
+	    private String title;
+	    private String status;
+	}
+
+}
